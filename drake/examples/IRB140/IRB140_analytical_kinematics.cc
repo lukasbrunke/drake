cfd03218da058c2fcf8035e32052318b0be70f19
hongkai-dai <daihongkai@gmail.com>
2017-03-20 10:38:57 -0700

    WIP: work on analytical kinematics

---
+++ drake/examples/IRB140/IRB140_analytical_kinematics.cc
@@ -0,0 +1,18 @@
+#include "drake/examples/IRB140/IRB140_analytical_kinematics.h"
+
+namespace drake {
+namespace examples {
+namespace IRB140 {
+IRB140AnalyticalKinematics::IRB140AnalyticalKinematics()
+    : l0(0.1095),
+      l1_x(0.07),
+      l1_y(0.2425),
+      l2(0.36),
+      l3(0.2185),
+      l4(0.1615),
+      l5(0.065) {
+
+}
+}
+}
+}
--- drake/examples/IRB140/IRB140_analytical_kinematics.h
+++ drake/examples/IRB140/IRB140_analytical_kinematics.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "drake/multibody/rigid_body_tree.h"
 
 #include <Eigen/Core>
@@ -11,12 +13,21 @@ namespace IRB140 {
  * [1] Introduction to Robotics by J. J. Craig, 2003
  */
 class IRB140AnalyticalKinematics {
- protected:
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(IRB140AnalyticalKinematics)
+
+  IRB140AnalyticalKinematics();
+
+  //
+  Eigen::Isometry3d R_01() const;
+ private:
   const double l0;  // offset of joint 1 in base link in the z direction.
   const double l1_x;  // offset of joint 2 in link 1 in the x direction.
   const double l1_y;  // offset of joint 2 in link 1 in the -y direction.
   const double l2;  // offset of joint 3 in link 2 in the -y direction.
-  const double 
+  const double l3;  // offset of joint 4 in link 3 in the x direction.
+  const double l4;  // offset of joint 5 in link 4 in the x direction.
+  const double l5;  // offset of joint 6 in link 5 in the x direction.
 };
 }
 }
