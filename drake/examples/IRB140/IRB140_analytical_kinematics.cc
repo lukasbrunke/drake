62ee5f126d36ccbf43dce2e64e80238cc0869308
hongkai-dai <daihongkai@gmail.com>
2017-03-20 13:51:06 -0700

    start to work on the test

--- drake/examples/IRB140/IRB140_analytical_kinematics.cc
+++ drake/examples/IRB140/IRB140_analytical_kinematics.cc
@@ -4,16 +4,26 @@ namespace drake {
 namespace examples {
 namespace IRB140 {
 IRB140AnalyticalKinematics::IRB140AnalyticalKinematics()
-    : l0(0.1095),
-      l1_x(0.07),
-      l1_y(0.2425),
-      l2(0.36),
-      l3(0.2185),
-      l4(0.1615),
-      l5(0.065) {}
+    : l0_(0.1095),
+      l1_x_(0.07),
+      l1_y_(0.2425),
+      l2_(0.36),
+      l3_(0.2185),
+      l4_(0.1615),
+      l5_(0.065) {}
 
 Eigen::Isometry3d IRB140AnalyticalKinematics::X_01(double theta) const {
+  Eigen::Isometry3d X;
+  X.linear() = (Eigen::AngleAxisd(-M_PI_2, Eigen::Vector3d(1, 0, 0)) * Eigen::AngleAxisd(theta, Eigen::Vector3d(0, -1, 0))).toRotationMatrix();
+  X.translation() = Eigen::Vector3d(0, 0, l0_);
+  return X;
+}
 
+Eigen::Isometry3d IRB140AnalyticalKinematics::X_12(double theta) const {
+  Eigen::Isometry3d X;
+  X.linear() = Eigen::AngleAxisd(theta, Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
+  X.translation() = Eigen::Vector3d(l1_x_, -l1_y_, 0);
+  return X;
 }
 }
 }
