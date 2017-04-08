21d5984b32f4e1116170cd033b77162f829825af
hongkai-dai <daihongkai@gmail.com>
2017-04-08 00:35:02 -0700

    Fix a bug in X_01

--- drake/examples/IRB140/IRB140_analytical_kinematics.h
+++ drake/examples/IRB140/IRB140_analytical_kinematics.h
@@ -25,36 +25,27 @@ class IRB140AnalyticalKinematics {
 
   RigidBodyTreed* robot() const {return robot_.get();}
 
-  // The pose of link 1 measured and expressed in link 0.
-  Eigen::Isometry3d X_01(double theta) const;
-
-  // The pose of link 2 measured and expressed in link 1.
-  Eigen::Isometry3d X_12(double theta) const;
-
-  // The pose of link 3 measured and expressed in link 2.
-  Eigen::Isometry3d X_23(double theta) const;
-
-  // The pose of link 4 measured and expressed in link 3.
-  Eigen::Isometry3d X_34(double theta) const;
-
-  // The pose of link 5 measured and expressed in link 4.
-  Eigen::Isometry3d X_45(double theta) const;
-
-  // The pose of link 6 measured and expressed in link 5.
-  Eigen::Isometry3d X_56(double theta) const;
-
   std::vector<Eigen::Matrix<double, 6, 1>> inverse_kinematics(const Eigen::Isometry3d& link6_pose);
 
   std::vector<double> q1(const Eigen::Isometry3d& link6_pose);
 
   std::vector<std::pair<double, double>> q23(const Eigen::Isometry3d& link6_pose, double q1);
 
-  Eigen::Matrix<symbolic::Expression, 4, 4> X_01_sym() const;
-  Eigen::Matrix<symbolic::Expression, 4, 4> X_12_sym() const;
-  Eigen::Matrix<symbolic::Expression, 4, 4> X_23_sym() const;
-  Eigen::Matrix<symbolic::Expression, 4, 4> X_34_sym() const;
-  Eigen::Matrix<symbolic::Expression, 4, 4> X_45_sym() const;
-  Eigen::Matrix<symbolic::Expression, 4, 4> X_56_sym() const;
+  Eigen::Matrix<symbolic::Expression, 4, 4> X_01() const;
+  Eigen::Matrix<symbolic::Expression, 4, 4> X_12() const;
+  Eigen::Matrix<symbolic::Expression, 4, 4> X_23() const;
+  Eigen::Matrix<symbolic::Expression, 4, 4> X_13() const;
+  Eigen::Matrix<symbolic::Expression, 4, 4> X_34() const;
+  Eigen::Matrix<symbolic::Expression, 4, 4> X_45() const;
+  Eigen::Matrix<symbolic::Expression, 4, 4> X_56() const;
+
+  Eigen::Isometry3d X_01(double theta) const;
+  Eigen::Isometry3d X_12(double theta) const;
+  Eigen::Isometry3d X_23(double theta) const;
+  Eigen::Isometry3d X_13(double theta2, double theta3) const;
+  Eigen::Isometry3d X_34(double theta) const;
+  Eigen::Isometry3d X_45(double theta) const;
+  Eigen::Isometry3d X_56(double theta) const;
 
  private:
   std::unique_ptr<RigidBodyTreed> robot_;
@@ -65,7 +56,6 @@ class IRB140AnalyticalKinematics {
   const double l3_;  // offset of joint 4 in link 3 in the x direction.
   const double l4_;  // offset of joint 5 in link 4 in the x direction.
 
- public:
   Eigen::Matrix<symbolic::Variable, 6, 1> c_;  // c_[i] is the cos of i'th joint angle.
   Eigen::Matrix<symbolic::Variable, 6, 1> s_;  // s_[i] is the sin of i'th joint angle.
   symbolic::Variable l0_var_;
@@ -74,6 +64,8 @@ class IRB140AnalyticalKinematics {
   symbolic::Variable l2_var_;
   symbolic::Variable l3_var_;
   symbolic::Variable l4_var_;
+  symbolic::Variable c23_var_;
+  symbolic::Variable s23_var_;
 };
 }  // namespace IRB140
 }  // namespace examples
