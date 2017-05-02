044d34fe2defce59e4ea96d6ed2516db872d914d
hongkai-dai <daihongkai@gmail.com>
2017-05-02 13:14:10 -0700

    Need to relax the feasibility tolerance to get gurobi 7 working for IRB140

--- drake/examples/IRB140/test/IRB140_global_ik_test.cc
+++ drake/examples/IRB140/test/IRB140_global_ik_test.cc
@@ -30,14 +30,48 @@ class GlobalIKTest : public ::testing::Test {
     global_ik_orient_cnstr_.constraint()->UpdateUpperBound(rotmat_flat);
   }
 
-  void SolveIK(const Eigen::Isometry3d& ee_pose) {
+  void SolveIK(const Eigen::Isometry3d& ee_pose, solvers::SolutionResult global_ik_status_expected) {
     q_analytical_ik_ = analytical_ik_.inverse_kinematics(ee_pose);
     SetEndEffectorPose(ee_pose);
     solvers::GurobiSolver gurobi_solver;
+    global_ik_.SetSolverOption(solvers::SolverType::kGurobi, "FeasibilityTol", 7E-6);
+    for (int i = 1; i < robot_->get_num_bodies(); ++i) {
+      const auto& body_R = global_ik_.body_rotation_matrix(i);
+      Eigen::Matrix<symbolic::Expression, 5, 1> cone_expr;
+      cone_expr(0) = 1.0;
+      cone_expr(1) = 3.0;
+      cone_expr.tail<3>() = body_R.col(0) + body_R.col(1) + body_R.col(2);
+      global_ik_.AddRotatedLorentzConeConstraint(cone_expr);
+      cone_expr.tail<3>() = body_R.col(0) + body_R.col(1) - body_R.col(2);
+      global_ik_.AddRotatedLorentzConeConstraint(cone_expr);
+      cone_expr.tail<3>() = body_R.col(0) - body_R.col(1) + body_R.col(2);
+      global_ik_.AddRotatedLorentzConeConstraint(cone_expr);
+      cone_expr.tail<3>() = body_R.col(0) - body_R.col(1) - body_R.col(2);
+      global_ik_.AddRotatedLorentzConeConstraint(cone_expr);
+      cone_expr.tail<3>() = body_R.row(0) + body_R.row(1) + body_R.row(2);
+      global_ik_.AddRotatedLorentzConeConstraint(cone_expr);
+      cone_expr.tail<3>() = body_R.row(0) + body_R.row(1) - body_R.row(2);
+      global_ik_.AddRotatedLorentzConeConstraint(cone_expr);
+      cone_expr.tail<3>() = body_R.row(0) - body_R.row(1) + body_R.row(2);
+      global_ik_.AddRotatedLorentzConeConstraint(cone_expr);
+      cone_expr.tail<3>() = body_R.row(0) - body_R.row(1) - body_R.row(2);
+      global_ik_.AddRotatedLorentzConeConstraint(cone_expr);
+    }
     solvers::SolutionResult global_ik_status = gurobi_solver.Solve(global_ik_);
-    EXPECT_FALSE(q_analytical_ik_.empty());
-    EXPECT_EQ(global_ik_status, solvers::SolutionResult::kSolutionFound);
-    q_global_ik_ = global_ik_.ReconstructGeneralizedPositionSolution();
+    switch (global_ik_status_expected) {
+      case solvers::SolutionResult::kSolutionFound:
+        EXPECT_FALSE(q_analytical_ik_.empty());
+        EXPECT_EQ(global_ik_status, solvers::SolutionResult::kSolutionFound);
+        q_global_ik_ = global_ik_.ReconstructGeneralizedPositionSolution();
+        break;
+      case solvers::SolutionResult::kInfeasibleConstraints:
+        EXPECT_TRUE(q_analytical_ik_.empty());
+        EXPECT_TRUE(global_ik_status == solvers::SolutionResult::kInfeasibleConstraints
+        || global_ik_status == solvers::SolutionResult::kInfeasible_Or_Unbounded);
+        break;
+      default:
+        throw std::runtime_error("Unknown status.");
+    }
   }
 
  private:
@@ -54,15 +88,35 @@ class GlobalIKTest : public ::testing::Test {
 TEST_F(GlobalIKTest, SinglePoseTest) {
   Eigen::Isometry3d ee_pose;
   ee_pose.linear() = Eigen::Matrix3d::Identity();
+  ee_pose.translation() << 0, -0.5, -0.1;
+  SolveIK(ee_pose, solvers::SolutionResult::kSolutionFound);
+
+  ee_pose.translation() << 0, -0.5, 0.1;
+  SolveIK(ee_pose, solvers::SolutionResult::kSolutionFound);
+
   ee_pose.translation() << 0.5, 0.2, 0.3;
-  SolveIK(ee_pose);
+  SolveIK(ee_pose, solvers::SolutionResult::kSolutionFound);
 
   ee_pose.translation() << 0, -0.2, 0.4;
-  SolveIK(ee_pose);
+  SolveIK(ee_pose, solvers::SolutionResult::kSolutionFound);
 
   ee_pose.linear() << Eigen::AngleAxisd(M_PI_2, Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
   ee_pose.translation() << 0.3, 0.1, 0.4;
-  SolveIK(ee_pose);
+  SolveIK(ee_pose, solvers::SolutionResult::kSolutionFound);
+}
+
+TEST_F(GlobalIKTest, InfeasibleSinglePoseTest) {
+  Eigen::Isometry3d ee_pose;
+  ee_pose.linear() = Eigen::Matrix3d::Identity();
+  ee_pose.translation() << 0.8, -0.3, 0.2;
+  SolveIK(ee_pose, solvers::SolutionResult::kInfeasibleConstraints);
+
+  ee_pose.translation() << 0, 0, 0.05;
+  SolveIK(ee_pose, solvers::SolutionResult::kInfeasibleConstraints);
+
+  ee_pose.linear() << Eigen::AngleAxisd(M_PI_2, Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
+  ee_pose.translation() << 0, -0.5, 0.4;
+  SolveIK(ee_pose, solvers::SolutionResult::kInfeasibleConstraints);
 }
 }
 }
