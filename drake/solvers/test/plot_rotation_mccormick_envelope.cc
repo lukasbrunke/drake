83d7c6a5b5aec257d9f1e113c9cc83cb93d5e490
hongkai-dai <daihongkai@gmail.com>
2017-05-23 17:35:02 -0700

    add drawing cutting planes

--- drake/solvers/test/plot_rotation_mccormick_envelope.cc
+++ drake/solvers/test/plot_rotation_mccormick_envelope.cc
@@ -12,6 +12,19 @@
 namespace drake {
 namespace solvers {
 namespace {
+void DrawCuttingPlane(double intersection, int axis) {
+  using common::CallMatlab;
+  Eigen::Matrix<double, 4, 3> plane_vert;
+  int axis1 = (axis + 1) % 3;
+  int axis2 = (axis + 2) % 3;
+  plane_vert.col(axis) = Eigen::Vector4d::Constant(intersection);
+  plane_vert.col(axis1) << -1, -1, 1, 1;
+  plane_vert.col(axis2) << -1, 1, 1, -1;
+  auto h = CallMatlab(1, "fill3", plane_vert.col(0), plane_vert.col(1), plane_vert.col(2), Eigen::RowVector3d(0.5, 0.2, 0.3));
+  CallMatlab("set", h[0], "FaceAlpha", 0.2);
+  //CallMatlab("set", h[0], "LineStyle", "none");
+}
+
 // This draws all the McCormick Envelope in the first orthant (+++).
 void DrawAllMcCormickEnvelopes(int num_bins) {
   for (int i = 0; i < num_bins; ++i) {
@@ -25,9 +38,15 @@ void DrawAllMcCormickEnvelopes(int num_bins) {
                              static_cast<double>(k + 1) / num_bins);
         if (bmin.norm() <= 1 && bmax.norm() >= 1) {
           DrawBoxSphereIntersection(bmin, bmax);
-          DrawBox(bmin, bmax);
+          // DrawBox(bmin, bmax);
         }
       }
+    }
+  }
+  for (int axis = 0; axis < 3; ++axis) {
+    for (int i = 0; i < num_bins; ++i) {
+      double intersection = static_cast<double>(i) / num_bins;
+      DrawCuttingPlane(intersection, axis);
     }
   }
 }
@@ -45,8 +64,8 @@ void DoMain() {
     CallMatlab("ylabel", "y");
     CallMatlab("zlabel", "z");
     CallMatlab("view", 145, 25);
-    std::string file_name = "/home/hongkai/research/ISRR2017/figure/sphere_" + std::to_string(num_bins) + "_bins_w_box";
-    CallMatlab("print", file_name, "-dsvg");
+    //std::string file_name = "/home/hongkai/research/ISRR2017/figure/sphere_" + std::to_string(num_bins) + "_bins_w_box";
+    //CallMatlab("print", file_name, "-dsvg");
   }
 }
 }  // namespace
