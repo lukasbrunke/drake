c9dd50a419d5dc6fac9f7dae443b0e5cee2ac5b1
hongkai-dai <daihongkai@gmail.com>
2017-12-03 18:24:01 -0800

    Add some utility function to visualize a posture of IRB140

--- drake/examples/irb140/test/irb140_common.cc
+++ drake/examples/irb140/test/irb140_common.cc
@@ -2,7 +2,12 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/find_resource.h"
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
+#include "drake/lcm/drake_lcm.h"
+#include "drake/multibody/parsers/urdf_parser.h"
+#include "drake/manipulation/util/simple_tree_visualizer.h"
+
 namespace drake {
 namespace examples {
 namespace IRB140 {
@@ -12,6 +17,29 @@ bool CompareIsometry3d(const Eigen::Isometry3d& X1, const Eigen::Isometry3d& X2,
   bool position_match = CompareMatrices(X1.translation(), X2.translation(), tol, MatrixCompareType::absolute);
   EXPECT_TRUE(CompareMatrices(X1.translation(), X2.translation(), tol, MatrixCompareType::absolute));
   return position_match && orientation_match;
+}
+
+std::unique_ptr<RigidBodyTreed> ConstructIRB140() {
+  std::unique_ptr<RigidBodyTree<double>> rigid_body_tree =
+      std::make_unique<RigidBodyTree<double>>();
+  const std::string model_path = FindResourceOrThrow("drake/examples/irb140/urdf/irb_140_shift.urdf");
+
+
+  parsers::urdf::AddModelInstanceFromUrdfFile(
+      model_path,
+      drake::multibody::joints::kFixed,
+      nullptr,
+      rigid_body_tree.get());
+
+  //AddFlatTerrainToWorld(rigid_body_tree.get());
+
+  return rigid_body_tree;
+}
+
+void VisualizePosture(const RigidBodyTreed& tree, const Eigen::Ref<const Eigen::VectorXd>& q) {
+  lcm::DrakeLcm lcm;
+  manipulation::SimpleTreeVisualizer simple_tree_visualizer(tree, &lcm);
+  simple_tree_visualizer.visualize(q);
 }
 }  // namespace IRB140
 }  // namespace examples
--- drake/examples/irb140/test/irb140_common.h
+++ drake/examples/irb140/test/irb140_common.h
@@ -1,10 +1,20 @@
+#pragma once
+
+#include <memory>
+
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+
+#include "drake/multibody/rigid_body_tree.h"
 
 namespace drake {
 namespace examples {
 namespace IRB140 {
 bool CompareIsometry3d(const Eigen::Isometry3d& X1, const Eigen::Isometry3d& X2, double tol = 1E-10);
+
+std::unique_ptr<RigidBodyTreed> ConstructIRB140();
+
+void VisualizePosture(const RigidBodyTreed& tree, const Eigen::Ref<const Eigen::VectorXd>& q);
 }  // namespace IRB140
 }  // namespace examples
 }  // namespace drake
