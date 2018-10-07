3e0c11e68f8efb6f61fe5d58eec43968fc39d327
hongkai-dai <daihongkai@gmail.com>
2018-10-07 16:06:55 -0700

    Use gurobi with multiple solutions.

--- attic/multibody/dev/test/stewart_platform_test.cc
+++ attic/multibody/dev/test/stewart_platform_test.cc
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 
+#include "drake/math/evenly_distributed_pts_on_sphere.h"
 #include "drake/math/rotation_matrix.h"
 #include "drake/solvers/gurobi_solver.h"
 #include "drake/solvers/mixed_integer_optimization_util.h"
@@ -23,12 +24,16 @@ void RemoveFileIfExist(const std::string& file_name) {
   file.close();
 }
 
-void WriteRowVector3dToFile(const Eigen::RowVector3d& value,
-                        const std::string& out_file_name) {
+template <typename Derived>
+void WriteRowVectorToFile(const Derived& value,
+                          const std::string& out_file_name) {
   std::fstream output_file;
   output_file.open(out_file_name, std::ios::app | std::ios::out);
   if (output_file.is_open()) {
-    output_file << value(0) << " " << value(1) << " " << value(2) << "\n";
+    for (int i = 0; i < value.size(); ++i) {
+      output_file << value(i) << " ";
+    }
+    output_file << "\n";
   }
   output_file.close();
 }
@@ -39,12 +44,45 @@ void AddLeg(const Eigen::Vector3d& p_PBi, const Eigen::Vector3d& p_BAi,
             const std::array<Eigen::VectorXd, 3>& phi_p_BPo,
             const std::array<VectorX<symbolic::Variable>, 3>& lambda_p_BPo,
             const Matrix3<symbolic::Expression>& p_BPo_times_R_BP,
-            solvers::MathematicalProgram* prog) {
+            bool use_linear_constraint, solvers::MathematicalProgram* prog) {
   // First add the constraint |p_BPo + R_BP * p_PBi - p_BAi| <= leg_length
-  Vector4<symbolic::Expression> v_lorentz;
-  v_lorentz(0) = leg_length;
-  v_lorentz.tail<3>() = p_BPo + R_BP * p_PBi - p_BAi;
-  prog->AddLorentzConeConstraint(v_lorentz);
+  const Vector3<symbolic::Expression> p_AiBi_B = p_BPo + R_BP * p_PBi - p_BAi;
+  if (use_linear_constraint) {
+    std::vector<Eigen::Vector3d> unit_vecs;
+    unit_vecs.emplace_back(1, 0, 0);
+    unit_vecs.emplace_back(0, 1, 0);
+    unit_vecs.emplace_back(0, 0, 1);
+    unit_vecs.emplace_back(std::sqrt(2) / 2, std::sqrt(2) / 2, 0);
+    unit_vecs.emplace_back(std::sqrt(2) / 2, -std::sqrt(2) / 2, 0);
+    unit_vecs.emplace_back(std::sqrt(2) / 2, 0, std::sqrt(2) / 2);
+    unit_vecs.emplace_back(std::sqrt(2) / 2, 0, -std::sqrt(2) / 2);
+    unit_vecs.emplace_back(0, std::sqrt(2) / 2, std::sqrt(2) / 2);
+    unit_vecs.emplace_back(0, std::sqrt(2) / 2, -std::sqrt(2) / 2);
+    unit_vecs.emplace_back(1 / std::sqrt(3), 1 / std::sqrt(3),
+                           1 / std::sqrt(3));
+    unit_vecs.emplace_back(1 / std::sqrt(3), -1 / std::sqrt(3),
+                           1 / std::sqrt(3));
+    unit_vecs.emplace_back(1 / std::sqrt(3), 1 / std::sqrt(3),
+                           -1 / std::sqrt(3));
+    unit_vecs.emplace_back(1 / std::sqrt(3), 1 / std::sqrt(3),
+                           -1 / std::sqrt(3));
+    for (const auto& unit_vec : unit_vecs) {
+      prog->AddLinearConstraint(unit_vec.dot(p_AiBi_B) <= leg_length);
+      prog->AddLinearConstraint(-unit_vec.dot(p_AiBi_B) <= leg_length);
+    }
+
+    const Eigen::Matrix3Xd unit_vectors =
+        math::UniformPtsOnSphereFibonacci(200);
+    for (int i = 0; i < unit_vectors.cols(); ++i) {
+      prog->AddLinearConstraint(unit_vectors.col(i).dot(p_AiBi_B) <=
+                                leg_length);
+    }
+  } else {
+    Vector4<symbolic::Expression> v_lorentz;
+    v_lorentz(0) = leg_length;
+    v_lorentz.tail<3>() = p_AiBi_B;
+    prog->AddLorentzConeConstraint(v_lorentz);
+  }
   // The constraint |p_BPo + R_BP * p_PBi - p_BAi| >= leg_length is equivalent
   // to
   // p_PBiᵀ * p_PBi + 2 (p_BPo - p_BAi)ᵀ * R_BP * p_PBi + (p_BPo - p_BAi)ᵀ *
@@ -76,23 +114,23 @@ void AddLeg(const Eigen::Vector3d& p_PBi, const Eigen::Vector3d& p_BAi,
 int DoMain() {
   solvers::MathematicalProgram prog;
   auto R_BP = solvers::NewRotationMatrixVars(&prog, "R");
-  solvers::AddRotationMatrixOrthonormalSocpConstraint(&prog, R_BP);
+  bool use_linear_constraint = true;
+  if (!use_linear_constraint) {
+    solvers::AddRotationMatrixOrthonormalSocpConstraint(&prog, R_BP);
+  }
   solvers::MixedIntegerRotationConstraintGenerator rotation_generator(
       solvers::MixedIntegerRotationConstraintGenerator::Approach::
           kBilinearMcCormick,
-      4, solvers::IntervalBinning::kLogarithmic);
+      8, solvers::IntervalBinning::kLogarithmic);
   const auto R_BP_return = rotation_generator.AddToProgram(R_BP, &prog);
   auto p_BPo = prog.NewContinuousVariables<3>("p");
 
   // We will have bilinear product p_BPo(i) * R_BP.col(j)(i) and the quadratic
   // product p_BPo(i) * p_BPo(i)
   std::array<Eigen::VectorXd, 3> phi_p_BPo;
-  phi_p_BPo[0].resize(9);
-  phi_p_BPo[0] << 0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1;
-  phi_p_BPo[1].resize(9);
-  phi_p_BPo[1] << -1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1;
-  phi_p_BPo[2].resize(9);
-  phi_p_BPo[2] << 0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1;
+  phi_p_BPo[0] = Eigen::VectorXd::LinSpaced(17, -0.5, 1.5);
+  phi_p_BPo[1] = Eigen::VectorXd::LinSpaced(17, -1, 1);
+  phi_p_BPo[2] = Eigen::VectorXd::LinSpaced(17, -0.5, 1);
 
   std::array<VectorX<symbolic::Variable>, 3> lambda_p_BPo;
   std::array<VectorX<symbolic::Variable>, 3> b_p_BPo;
@@ -144,20 +182,29 @@ int DoMain() {
   leg_length[5] = 0.771071;
   for (int i = 0; i < 6; ++i) {
     AddLeg(p_PBi[i], p_BAi[i], leg_length[i], R_BP, p_BPo, phi_p_BPo,
-           lambda_p_BPo, p_BPo_times_R_BP, &prog);
+           lambda_p_BPo, p_BPo_times_R_BP, use_linear_constraint, &prog);
+  }
+
+  if (use_linear_constraint) {
+    assert(prog.lorentz_cone_constraints().empty());
+    assert(prog.rotated_lorentz_cone_constraints().empty());
   }
 
   solvers::GurobiSolver solver;
   prog.SetSolverOption(solvers::GurobiSolver::id(), "OutputFlag", 1);
   prog.SetSolverOption(solvers::GurobiSolver::id(), "PoolSearchMode", 2);
-  const int multiple_sol_count = 40;
+  const int multiple_sol_count = 150;
   prog.SetSolverOption(solvers::GurobiSolver::id(), "PoolSolutions",
                        multiple_sol_count);
   const auto result = solver.Solve(prog);
   std::cout << result << "\n";
 
-  const std::string file_name("stewart_platform_position.txt");
-  RemoveFileIfExist(file_name);
+  const std::string position_file_name("stewart_platform_position.txt");
+  RemoveFileIfExist(position_file_name);
+  const std::string orientation_file_name("stewart_platform_orientation.txt");
+  RemoveFileIfExist(orientation_file_name);
+  const std::string result_file_name("stewart_platform_result.txt");
+  RemoveFileIfExist(result_file_name);
 
   for (int count = 0; count < multiple_sol_count; ++count) {
     const Eigen::Matrix3d R_BP_sol = prog.GetSuboptimalSolution(R_BP, count);
@@ -166,13 +213,22 @@ int DoMain() {
             .matrix();
     const Eigen::Vector3d p_BPo_sol = prog.GetSuboptimalSolution(p_BPo, count);
     std::cout << "\np_BPo_sol : " << p_BPo_sol.transpose() << "\n";
-    WriteRowVector3dToFile(p_BPo_sol.transpose(), file_name);
+    WriteRowVectorToFile(p_BPo_sol.transpose(), position_file_name);
+    for (int i = 0; i < 3; ++i) {
+      WriteRowVectorToFile(R_BP_proj.row(i), orientation_file_name);
+    }
     for (int i = 0; i < 6; ++i) {
       const Eigen::Vector3d p_BBi = p_BPo_sol + R_BP_proj * p_PBi[i];
       const double leg_length_sol = (p_BBi - p_BAi[i]).norm();
       std::cout << "leg " << i << " length_sol: " << leg_length_sol
                 << " length " << leg_length[i] << "\n";
     }
+    const Eigen::Vector3d p_A1B1_B =
+        p_BPo_sol + R_BP_proj * p_PBi[0] - p_BAi[0];
+    const Eigen::Vector3d n1 = p_A1B1_B.normalized();
+    Eigen::VectorXd sol(12);
+    sol << n1, R_BP_proj.col(0), R_BP_proj.col(1), R_BP_proj.col(2);
+    WriteRowVectorToFile(sol.transpose(), result_file_name);
   }
 
   return 0;
