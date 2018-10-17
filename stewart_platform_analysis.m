f1e238b076c8fe3abd0aabbc6042f12e1173a3f7
hongkai-dai <daihongkai@gmail.com>
2018-10-16 17:35:31 -0700

    also visualize the approximated solutions that are not in the neighbourhood of the analytical solutions.

--- stewart_platform_analysis.m
+++ stewart_platform_analysis.m
@@ -1,7 +1,17 @@
 result = load('stewart_platform_36_binary/stewart_platform_result.txt');
 analytical_result = load('stewart_platform_analytical_result.txt');
+% match(i) stores the best match in result that matches
+% analytical_result(i, :)
 match = ones(40, 1);
 match_error = zeros(40, 1);
 for i = 1: 40
   [match_error(i), match(i)] = min(sum((result - bsxfun(@times, ones(size(result, 1), 1), analytical_result(i, :))).^2, 2));
+end
+
+% not_match(i) stores the best match in analytical_result that matches
+% result(i, :)
+not_match = ones(size(result, 1), 1);
+not_match_error = ones(size(result, 1), 1);
+for i = 1 : size(result, 1)
+    [not_match_error(i), not_match(i)] = min(sum(((bsxfun(@times, result(i,:), ones(40, 1)) - analytical_result)).^2, 2));
 end
