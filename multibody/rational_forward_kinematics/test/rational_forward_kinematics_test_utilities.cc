77485be798e2202eb52343441a827001395e353d
hongkai-dai <daihongkai@gmail.com>
2018-12-10 19:42:53 -0800

    Rebase on master. Use generate monomial basis with given order to compute the monomial basis. AddSosConstraint takes too much time.

--- multibody/rational_forward_kinematics/test/rational_forward_kinematics_test_utilities.cc
+++ multibody/rational_forward_kinematics/test/rational_forward_kinematics_test_utilities.cc
@@ -2,7 +2,7 @@
 
 #include "drake/common/find_resource.h"
 #include "drake/multibody/benchmarks/kuka_iiwa_robot/make_kuka_iiwa_model.h"
-#include "drake/multibody/multibody_tree/parsing/multibody_plant_sdf_parser.h"
+#include "drake/multibody/parsing/parser.h"
 
 namespace drake {
 namespace multibody {
@@ -11,7 +11,8 @@ std::unique_ptr<multibody_plant::MultibodyPlant<double>> ConstructIiwaPlant(
   const std::string file_path =
       "drake/manipulation/models/iiwa_description/sdf/" + iiwa_sdf_name;
   auto plant = std::make_unique<multibody_plant::MultibodyPlant<double>>(0);
-  parsing::AddModelFromSdfFile(FindResourceOrThrow(file_path), plant.get());
+  Parser parser(plant.get());
+  parser.AddModelFromFile(FindResourceOrThrow(file_path));
   plant->WeldFrames(plant->world_frame(), plant->GetFrameByName("iiwa_link_0"));
   plant->Finalize();
   return plant;
