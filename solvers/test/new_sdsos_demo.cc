9242ba5431e1017dc3af58147a70ba594cb487f5
hongkai-dai <daihongkai@gmail.com>
2018-12-14 08:40:43 -0800

    Add a test to profile new sdsos polynomial.

---
+++ solvers/test/new_sdsos_demo.cc
@@ -0,0 +1,24 @@
+#include "drake/solvers/mathematical_program.h"
+#include "drake/common/symbolic_monomial_util.h"
+#include <ctime>
+
+namespace drake {
+namespace solvers {
+int DoMain() {
+  MathematicalProgram prog;
+  constexpr int kNumT = 4;
+  auto t = prog.NewIndeterminates<kNumT>();
+  const auto m = symbolic::MonomialBasis<kNumT, 3>(symbolic::Variables(t));
+  std::cout << "add sdsos polynomial.\n";
+  std::clock_t start = clock();
+  prog.NewNonnegativePolynomial(m, MathematicalProgram::NonnegativePolynomial::kSdsos);
+  double duration = (clock() - start) / static_cast<double>(CLOCKS_PER_SEC);
+  std::cout << "duration (s): " << duration << "\n";
+  return 0;
+}
+}  // namespace solvers
+}  // namespace drake
+
+int main() {
+  return drake::solvers::DoMain();
+}
