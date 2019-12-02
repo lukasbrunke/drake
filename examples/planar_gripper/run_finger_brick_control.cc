c3abe0f395b2ab95272e662e1de8ab6d4958912c
hongkai-dai <daihongkai@gmail.com>
2019-12-02 09:20:09 -0800

    WIP: add a diagram for QP controller.

---
+++ examples/planar_gripper/run_finger_brick_control.cc
@@ -0,0 +1,28 @@
+#include <gflags/gflags.h>
+
+#include "drake/common/find_resource.h"
+#include "drake/examples/planar_gripper/finger_brick_control.h"
+#include "drake/geometry/scene_graph.h"
+#include "drake/multibody/parsing/parser.h"
+#include "drake/multibody/plant/contact_results_to_lcm.h"
+#include "drake/multibody/plant/multibody_plant.h"
+#include "drake/multibody/tree/prismatic_joint.h"
+#include "drake/multibody/tree/revolute_joint.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/framework/diagram_builder.h"
+
+namespace drake {
+namespace examples {
+namespace planar_gripper {
+int DoMain() {
+}
+}  // namespace planar_gripper
+}  // namespace examples
+}  // namespace drake
+
+int main(int argc, char* argv[]) {
+  gflags::SetUsageMessage(
+      "A simple planar gripper example.");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  return drake::examples::planar_gripper::do_main();
+}
