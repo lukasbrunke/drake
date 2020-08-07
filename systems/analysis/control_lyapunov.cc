7e562ef4447fcf3a0abbf1b29f0d8e172967269b
hongkai-dai <daihongkai@gmail.com>
2020-08-06 22:17:19 -0700

    Implement MaximizeEpsGivenVBoxInputBound. Given an initial guess of V, find the Lagrangian multipliers, together with the slack variable to maximize the exponential convergence rate eps. When maximizing eps, mosek runs into numerical problem. But if we impose a constraint like eps >= 3, then mosek runs fine. Need to investigate into this.

--- systems/analysis/control_lyapunov.cc
+++ systems/analysis/control_lyapunov.cc
@@ -45,6 +45,94 @@ symbolic::Polynomial VdotCalculator::Calc(
   return dVdx_times_f_ + (dVdx_times_G_ * u)(0);
 }
 
+void CheckDynamicsInput(const symbolic::Polynomial& V,
+                        const VectorX<symbolic::Polynomial>& f,
+                        const MatrixX<symbolic::Polynomial>& G,
+                        const symbolic::Variables& x_set) {
+  DRAKE_DEMAND(V.indeterminates().IsSubsetOf(x_set));
+  DRAKE_DEMAND(f.rows() == static_cast<long>(x_set.size()));
+  DRAKE_DEMAND(f.rows() == G.rows());
+  for (int i = 0; i < static_cast<int>(f.rows()); ++i) {
+    DRAKE_DEMAND(f[i].indeterminates().IsSubsetOf(x_set));
+    for (int j = 0; j < static_cast<int>(G.cols()); ++j) {
+      DRAKE_DEMAND(G(i, j).indeterminates().IsSubsetOf(x_set));
+    }
+  }
+}
+MaximizeEpsGivenVBoxInputBound::MaximizeEpsGivenVBoxInputBound(
+    symbolic::Polynomial V, VectorX<symbolic::Polynomial> f,
+    MatrixX<symbolic::Polynomial> G,
+    const std::vector<std::array<symbolic::Polynomial, 2>>& l_given,
+    std::vector<std::array<int, 6>> lagrangian_degrees,
+    std::vector<int> b_degrees, VectorX<symbolic::Variable> x)
+    : prog_{},
+      V_{std::move(V)},
+      f_{std::move(f)},
+      G_{std::move(G)},
+      nx_{static_cast<int>(f_.rows())},
+      nu_{static_cast<int>(G_.cols())},
+      l_{static_cast<size_t>(nu_)},
+      lagrangian_degrees_{std::move(lagrangian_degrees)},
+      b_degrees_{std::move(b_degrees)},
+      x_{std::move(x)},
+      b_{nu_},
+      constraint_grams_{static_cast<size_t>(nu_)} {
+  const symbolic::Variables x_set(x_);
+  prog_.AddIndeterminates(x_);
+  CheckDynamicsInput(V_, f_, G_, x_set);
+  DRAKE_DEMAND(static_cast<int>(b_degrees_.size()) == nu_);
+
+  // Add Lagrangian decision variables.
+  for (int i = 0; i < nu_; ++i) {
+    DRAKE_DEMAND(l_given[i][0].TotalDegree() == lagrangian_degrees_[i][0]);
+    DRAKE_DEMAND(l_given[i][1].TotalDegree() == lagrangian_degrees_[i][1]);
+    l_[i][0] = l_given[i][0];
+    l_[i][1] = l_given[i][1];
+    for (int j = 2; j < 6; ++j) {
+      MatrixX<symbolic::Variable> lagrangian_gram;
+      std::tie(l_[i][j], lagrangian_gram) =
+          prog_.NewSosPolynomial(x_set, lagrangian_degrees_[i][j]);
+    }
+  }
+
+  eps_ = prog_.NewContinuousVariables<1>("eps")(0);
+
+  const RowVectorX<symbolic::Polynomial> dVdx = V_.Jacobian(x_);
+  // Since we will add the constraint ∂V/∂x*f(x) + εV = ∑ᵢ bᵢ(x), we know that
+  // the highest degree of b should be at least degree(∂V/∂x*f(x) + εV).
+  const symbolic::Polynomial dVdx_times_f = (dVdx * f_)(0);
+  if (*std::max_element(b_degrees_.begin(), b_degrees_.end()) <
+      std::max(dVdx_times_f.TotalDegree(), V.TotalDegree())) {
+    throw std::invalid_argument("The degree of b is too low.");
+  }
+
+  // Add free polynomial b
+  for (int i = 0; i < nu_; ++i) {
+    b_(i) = prog_.NewFreePolynomial(x_set, b_degrees_[i], "b");
+  }
+
+  // Add the constraint ∂V/∂x*f(x) + εV = ∑ᵢ bᵢ(x)
+  prog_.AddEqualityConstraintBetweenPolynomials(b_.sum(),
+                                                dVdx_times_f + eps_ * V_);
+  // Add the constraint
+  // (lᵢ₁(x)+1)(∂V/∂x*Gᵢ(x)−bᵢ(x)) − lᵢ₃(x)*∂V/∂x*Gᵢ(x) - lᵢ₅(x)*(1 − V) >= 0
+  // (lᵢ₂(x)+1)(−∂V/∂x*Gᵢ(x)−bᵢ(x)) + lᵢ₄(x)*∂V/∂x*Gᵢ(x) - lᵢ₆(x)*(1 − V) >= 0
+  for (int i = 0; i < nu_; ++i) {
+    const symbolic::Polynomial dVdx_times_Gi = (dVdx * G_.col(i))(0);
+    const symbolic::Polynomial p1 = (l_[i][0] + 1) * (dVdx_times_Gi - b_(i)) -
+                                    l_[i][2] * dVdx_times_Gi -
+                                    l_[i][4] * (1 - V_);
+    const symbolic::Polynomial p2 = (l_[i][1] + 1) * (-dVdx_times_Gi - b_(i)) +
+                                    l_[i][3] * dVdx_times_Gi -
+                                    l_[i][5] * (1 - V_);
+    constraint_grams_[i][0] = prog_.AddSosConstraint(p1);
+    constraint_grams_[i][1] = prog_.AddSosConstraint(p2);
+  }
+
+  // prog_.AddLinearCost(-eps_);
+  prog_.AddLinearConstraint(eps_ >= 3);
+}
+
 SearchLagrangianGivenVBoxInputBound::SearchLagrangianGivenVBoxInputBound(
     symbolic::Polynomial V, VectorX<symbolic::Polynomial> f,
     MatrixX<symbolic::Polynomial> G, VectorX<symbolic::Polynomial> b,
@@ -61,18 +149,11 @@ SearchLagrangianGivenVBoxInputBound::SearchLagrangianGivenVBoxInputBound(
       l_{static_cast<size_t>(nu_)},
       lagrangian_degrees_{std::move(lagrangian_degrees)},
       lagrangian_grams_{static_cast<size_t>(nu_)} {
-  DRAKE_DEMAND(G.rows() == nx_);
-  DRAKE_DEMAND(b.rows() == nu_);
   const symbolic::Variables x_set{x_};
-  DRAKE_DEMAND(V.indeterminates().IsSubsetOf(x_set));
-  for (int i = 0; i < nx_; ++i) {
-    DRAKE_DEMAND(f_(i).indeterminates().IsSubsetOf(x_set));
-    for (int j = 0; j < nu_; ++j) {
-      DRAKE_DEMAND(G_(i, j).indeterminates().IsSubsetOf(x_set));
-    }
-  }
+  CheckDynamicsInput(V_, f_, G_, x_set);
+  DRAKE_DEMAND(b.rows() == nu_);
   for (int i = 0; i < nu_; ++i) {
-    DRAKE_DEMAND(b(i).indeterminates().IsSubsetOf(x_set));
+    DRAKE_DEMAND(b_(i).indeterminates().IsSubsetOf(x_set));
   }
   prog_.AddIndeterminates(x_);
   for (int i = 0; i < nu_; ++i) {
@@ -82,18 +163,18 @@ SearchLagrangianGivenVBoxInputBound::SearchLagrangianGivenVBoxInputBound(
     }
   }
 
-  const auto dVdx = V.Jacobian(x);
+  const auto dVdx = V_.Jacobian(x_);
   // Now impose the constraint
   // (lᵢ₁(x)+1)(∂V/∂x*Gᵢ(x)−bᵢ(x)) − lᵢ₃(x)*∂V/∂x*Gᵢ(x) - lᵢ₅(x)*(1 − V) >= 0
   // (lᵢ₂(x)+1)(−∂V/∂x*Gᵢ(x)−bᵢ(x)) + lᵢ₄(x)*∂V/∂x*Gᵢ(x) - lᵢ₆(x)*(1 − V) >= 0
   for (int i = 0; i < nu_; ++i) {
-    const symbolic::Polynomial dVdx_times_Gi = dVdx * G.col(i);
+    const symbolic::Polynomial dVdx_times_Gi = dVdx * G_.col(i);
     const symbolic::Polynomial p1 =
-        (l_[i][0] + 1) * (dVdx_times_Gi - b(i)) -
-        l_[i][2] * dVdx_times_Gi * l_[i][4] * (1 - V);
-    const symbolic::Polynomial p2 = (l_[i][1] + 1) * (-dVdx_times_Gi - b(i)) +
+        (l_[i][0] + 1) * (dVdx_times_Gi - b_(i)) -
+        l_[i][2] * dVdx_times_Gi * l_[i][4] * (1 - V_);
+    const symbolic::Polynomial p2 = (l_[i][1] + 1) * (-dVdx_times_Gi - b_(i)) +
                                     l_[i][3] * dVdx_times_Gi -
-                                    l_[i][5] * (1 - V);
+                                    l_[i][5] * (1 - V_);
     prog_.AddSosConstraint(p1);
     prog_.AddSosConstraint(p2);
   }
--- systems/analysis/test/control_lyapunov_test.cc
+++ systems/analysis/test/control_lyapunov_test.cc
@@ -3,6 +3,9 @@
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/symbolic_test_util.h"
+#include "drake/solvers/csdp_solver.h"
+#include "drake/solvers/mosek_solver.h"
+#include "drake/solvers/solve.h"
 #include "drake/systems/controllers/linear_quadratic_regulator.h"
 
 namespace drake {
@@ -58,7 +61,101 @@ class SimpleLinearSystemTest : public ::testing::Test {
   Vector2<symbolic::Variable> x_;
 };
 
-TEST_F(SimpleLinearSystemTest, SearchLagrangianGivenVBoxInputBound) {
+void CheckMaximizeEpsGivenVBoxInputBoundResult(
+    const MaximizeEpsGivenVBoxInputBound& dut,
+    const solvers::MathematicalProgramResult& result,
+    const symbolic::Polynomial& V, const VectorX<symbolic::Polynomial>& f,
+    const MatrixX<symbolic::Polynomial>& G,
+    const VectorX<symbolic::Variable>& x, double tol) {
+  ASSERT_TRUE(result.is_success());
+  const RowVectorX<symbolic::Polynomial> dVdx = V.Jacobian(x);
+  const double eps_val = result.GetSolution(dut.eps());
+  const int nu = G.cols();
+  VectorX<symbolic::Polynomial> b_result(nu);
+  for (int i = 0; i < nu; ++i) {
+    b_result(i) = result.GetSolution(dut.b()(i));
+  }
+
+  EXPECT_TRUE(symbolic::test::PolynomialEqual((dVdx * f)(0) + eps_val * V,
+                                              b_result.sum(), tol));
+  std::vector<std::array<symbolic::Polynomial, 6>> lagrangians_result(nu);
+  for (int i = 0; i < nu; ++i) {
+    for (int j = 0; j < 6; ++j) {
+      lagrangians_result[i][j] = result.GetSolution(dut.lagrangians()[i][j]);
+    }
+    const symbolic::Polynomial dVdx_times_Gi = (dVdx * G.col(i))(0);
+    const symbolic::Polynomial p1 =
+        (lagrangians_result[i][0] + 1) * (dVdx_times_Gi - b_result(i)) -
+        lagrangians_result[i][2] * dVdx_times_Gi -
+        lagrangians_result[i][4] * (1 - V);
+    const VectorX<symbolic::Monomial>& monomials1 =
+        dut.constraint_grams()[i][0].second;
+    const Eigen::MatrixXd grams1 =
+        result.GetSolution(dut.constraint_grams()[i][0].first);
+    EXPECT_TRUE(symbolic::test::PolynomialEqual(
+        p1, monomials1.dot(grams1 * monomials1), tol));
+    const symbolic::Polynomial p2 =
+        (lagrangians_result[i][1] + 1) * (-dVdx_times_Gi - b_result(i)) +
+        lagrangians_result[i][3] * dVdx_times_Gi -
+        lagrangians_result[i][5] * (1 - V);
+    const VectorX<symbolic::Monomial>& monomials2 =
+        dut.constraint_grams()[i][1].second;
+    const Eigen::MatrixXd grams2 =
+        result.GetSolution(dut.constraint_grams()[i][1].first);
+    EXPECT_TRUE(symbolic::test::PolynomialEqual(
+        p2, monomials2.dot(grams2 * monomials2), tol));
+
+    // Now check if the gram matrices are psd.
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es;
+    es.compute(grams1);
+    EXPECT_TRUE((es.eigenvalues().array() >= -tol).all());
+    es.compute(grams2);
+    EXPECT_TRUE((es.eigenvalues().array() >= -tol).all());
+  }
+}
+
+// Sample many points inside the level set V(x) <= 1, and verify that min_u
+// Vdot(x, u) (-1 <= u <= 1) is less than -eps * V.
+void ValidateRegionOfAttractionBySample(const VectorX<symbolic::Polynomial>& f,
+                                        const MatrixX<symbolic::Polynomial>& G,
+                                        const symbolic::Polynomial& V,
+                                        const VectorX<symbolic::Variable>& x,
+                                        const Eigen::MatrixXd& u_vertices,
+                                        double eps, int num_samples,
+                                        double abs_tol, double rel_tol) {
+  int sample_count = 0;
+  const int nx = f.rows();
+  const int nu = G.cols();
+  const RowVectorX<symbolic::Polynomial> dVdx = V.Jacobian(x);
+  const symbolic::Polynomial dVdx_times_f = (dVdx * f)(0);
+  const RowVectorX<symbolic::Polynomial> dVdx_times_G = dVdx * G;
+  const int num_u_vertices = u_vertices.cols();
+  while (sample_count < num_samples) {
+    const Eigen::VectorXd x_val = Eigen::VectorXd::Random(nx);
+    symbolic::Environment env;
+    env.insert(x, x_val);
+    const double V_val = V.Evaluate(env);
+    EXPECT_GE(V_val, -abs_tol);
+    if (V_val <= 1) {
+      const double dVdx_times_f_val = dVdx_times_f.Evaluate(env);
+      Eigen::RowVectorXd dVdx_times_G_val(nu);
+      for (int i = 0; i < nu; ++i) {
+        dVdx_times_G_val(i) = dVdx_times_G(i).Evaluate(env);
+      }
+      const double Vdot =
+          (dVdx_times_f_val * Eigen::RowVectorXd::Ones(num_u_vertices) +
+           dVdx_times_G_val * u_vertices)
+              .array()
+              .minCoeff();
+      EXPECT_TRUE(-Vdot / V_val > eps - rel_tol ||
+                  -Vdot > V_val * eps - abs_tol);
+
+      sample_count++;
+    }
+  }
+}
+
+TEST_F(SimpleLinearSystemTest, MaximizeEpsGivenVBoxInputBound) {
   // We compute the LQR cost-to-go as the candidate Lyapunov function.
   const controllers::LinearQuadraticRegulatorResult lqr_result =
       controllers::LinearQuadraticRegulator(A_, B_, Eigen::Matrix2d::Identity(),
@@ -68,7 +165,7 @@ TEST_F(SimpleLinearSystemTest, SearchLagrangianGivenVBoxInputBound) {
   // We multiply the LQR cost-to-go by a factor (10 here), so that we start with
   // a very small neighbourhood around the origin as the initial guess of ROA
   // V(x) <= 1
-  const symbolic::Polynomial V(x_.dot(10 * lqr_result.S * x_), x_set);
+  const symbolic::Polynomial V(x_.dot(100 * lqr_result.S * x_), x_set);
 
   Vector2<symbolic::Polynomial> f;
   f[0] = symbolic::Polynomial(A_.row(0).dot(x_), x_set);
@@ -79,6 +176,41 @@ TEST_F(SimpleLinearSystemTest, SearchLagrangianGivenVBoxInputBound) {
       G(i, j) = symbolic::Polynomial(B_(i, j));
     }
   }
+  // Set l_[i][0] and l_[i][1] to 1 + x.dot(x)
+  const int nu = 2;
+  std::vector<std::array<symbolic::Polynomial, 2>> l_given{nu};
+  for (int i = 0; i < nu; ++i) {
+    l_given[i][0] =
+        symbolic::Polynomial(1 + x_.cast<symbolic::Expression>().dot(x_));
+    l_given[i][1] =
+        symbolic::Polynomial(1 + x_.cast<symbolic::Expression>().dot(x_));
+  }
+  std::vector<std::array<int, 6>> lagrangian_degrees{nu};
+  for (int i = 0; i < nu; ++i) {
+    lagrangian_degrees[i][0] = 2;
+    lagrangian_degrees[i][1] = 2;
+    for (int j = 2; j < 6; ++j) {
+      lagrangian_degrees[i][j] = 2;
+    }
+  }
+
+  std::vector<int> b_degrees(nu, 2);
+
+  MaximizeEpsGivenVBoxInputBound dut(V, f, G, l_given, lagrangian_degrees,
+                                     b_degrees, x_);
+
+  solvers::MosekSolver mosek_solver;
+  solvers::CsdpSolver csdp_solver;
+  mosek_solver.set_stream_logging(true, "");
+  const auto result = csdp_solver.Solve(dut.prog());
+  EXPECT_TRUE(result.is_success());
+  CheckMaximizeEpsGivenVBoxInputBoundResult(dut, result, V, f, G, x_, 1.3E-5);
+
+  const double eps = 3;
+  Eigen::Matrix<double, 2, 4> u_vertices;
+  u_vertices << 1, 1, -1, -1, 1, -1, 1, -1;
+  ValidateRegionOfAttractionBySample(f, G, V, x_, u_vertices, eps, 100, 1E-5,
+                                     1E-3);
 }
 
 }  // namespace analysis
