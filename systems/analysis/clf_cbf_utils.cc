cc1823a9b1441f89fce25ed8e99d86ee1fa80bad
hongkai-dai <daihongkai@gmail.com>
2022-05-21 10:22:49 -0700

    Move MaximizeInnerEllipsoidRho to clf_cbf_utils

--- systems/analysis/clf_cbf_utils.cc
+++ systems/analysis/clf_cbf_utils.cc
@@ -117,6 +117,126 @@ solvers::MathematicalProgramResult SearchWithBackoff(
   return result;
 }
 
+void MaximizeInnerEllipsoidRho(
+    const Eigen::Ref<const VectorX<symbolic::Variable>>& x,
+    const Eigen::Ref<const Eigen::VectorXd>& x_star,
+    const Eigen::Ref<const Eigen::MatrixXd>& S, const symbolic::Polynomial& f,
+    const symbolic::Polynomial& t, int s_degree,
+    const solvers::SolverId& solver_id,
+    const std::optional<solvers::SolverOptions>& solver_options,
+    double backoff_scale, double* rho_sol, symbolic::Polynomial* s_sol) {
+  solvers::MathematicalProgram prog;
+  prog.AddIndeterminates(x);
+  auto rho = prog.NewContinuousVariables<1>("rho")(0);
+  const symbolic::Variables x_set(x);
+  symbolic::Polynomial s;
+  if (s_degree == 0) {
+    const auto s_constant = prog.NewContinuousVariables<1>("s_constant")(0);
+    prog.AddBoundingBoxConstraint(0, kInf, s_constant);
+    s = symbolic::Polynomial({{symbolic::Monomial(), s_constant}});
+  } else {
+    std::tie(s, std::ignore) = prog.NewSosPolynomial(x_set, s_degree);
+  }
+
+  const symbolic::Polynomial ellipsoid_poly =
+      internal::EllipsoidPolynomial<symbolic::Variable>(x, x_star, S, rho);
+  const symbolic::Polynomial sos_poly = (1 + t) * ellipsoid_poly - s * f;
+  prog.AddSosConstraint(sos_poly);
+  prog.AddLinearCost(-rho);
+  const auto result =
+      SearchWithBackoff(&prog, solver_id, solver_options, backoff_scale);
+  DRAKE_DEMAND(result.is_success());
+  *rho_sol = result.GetSolution(rho);
+  *s_sol = result.GetSolution(s);
+}
+
+void MaximizeInnerEllipsoidRho(
+    const Eigen::Ref<const VectorX<symbolic::Variable>>& x,
+    const Eigen::Ref<const Eigen::VectorXd>& x_star,
+    const Eigen::Ref<const Eigen::MatrixXd>& S, const symbolic::Polynomial& f,
+    int r_degree, double rho_max, double rho_min,
+    const solvers::SolverId& solver_id,
+    const std::optional<solvers::SolverOptions>& solver_options, double rho_tol,
+    double* rho_sol, symbolic::Polynomial* r_sol) {
+  DRAKE_DEMAND(rho_max > rho_min);
+  DRAKE_DEMAND(rho_tol > 0);
+  const symbolic::Polynomial ellipsoid_quadratic =
+      internal::EllipsoidPolynomial(x, x_star, S, 0.);
+  auto is_feasible = [&x, &f, &r_degree, &solver_id, &solver_options,
+                      &ellipsoid_quadratic, r_sol](double rho) {
+    solvers::MathematicalProgram prog;
+    prog.AddIndeterminates(x);
+    symbolic::Polynomial r;
+    std::tie(r, std::ignore) =
+        prog.NewSosPolynomial(symbolic::Variables(x), r_degree);
+    prog.AddSosConstraint(-f - r * (rho - ellipsoid_quadratic));
+    auto solver = solvers::MakeSolver(solver_id);
+    solvers::MathematicalProgramResult result;
+    solver->Solve(prog, std::nullopt, solver_options, &result);
+    if (result.is_success()) {
+      *r_sol = result.GetSolution(r);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  if (!is_feasible(rho_min)) {
+    drake::log()->error("MaximizeEllipsoidRho: rho_min={} is infeasible",
+                        rho_min);
+  }
+  if (is_feasible(rho_max)) {
+    *rho_sol = rho_max;
+    return;
+  }
+  while (rho_max - rho_min > rho_tol) {
+    const double rho_mid = (rho_max + rho_min) / 2;
+    if (is_feasible(rho_mid)) {
+      rho_min = rho_mid;
+    } else {
+      rho_max = rho_mid;
+    }
+  }
+  *rho_sol = rho_min;
+}
+
+namespace internal {
+template <typename RhoType>
+symbolic::Polynomial EllipsoidPolynomial(
+    const Eigen::Ref<const VectorX<symbolic::Variable>>& x,
+    const Eigen::Ref<const Eigen::VectorXd>& x_star,
+    const Eigen::Ref<const Eigen::MatrixXd>& S, const RhoType& rho) {
+  // The ellipsoid polynomial (x−x*)ᵀS(x−x*)−ρ
+  symbolic::Polynomial::MapType ellipsoid_poly_map;
+  // Add constant term x*ᵀ*S*x* - ρ
+  ellipsoid_poly_map.emplace(symbolic::Monomial(),
+                             x_star.dot(S * x_star) - rho);
+  const Eigen::VectorXd S_times_x_star = (S + S.transpose()) / 2 * x_star;
+  for (int i = 0; i < x.rows(); ++i) {
+    // Add S(i, i) * x(i)²
+    ellipsoid_poly_map.emplace(symbolic::Monomial(x(i), 2), S(i, i));
+    // Add -2 * x_starᵀ * S.col(i) * x(i)
+    ellipsoid_poly_map.emplace(symbolic::Monomial(x(i)),
+                               -2 * S_times_x_star(i));
+    for (int j = i + 1; j < x.rows(); ++j) {
+      // Add 2*S(i, j) * x(i) * x(j)
+      ellipsoid_poly_map.emplace(symbolic::Monomial({{x(i), 1}, {x(j), 1}}),
+                                 S(i, j) + S(j, i));
+    }
+  }
+  return symbolic::Polynomial{ellipsoid_poly_map};
+}
+
+// Explicit instantiation
+template symbolic::Polynomial EllipsoidPolynomial<double>(
+    const Eigen::Ref<const VectorX<symbolic::Variable>>&,
+    const Eigen::Ref<const Eigen::VectorXd>&,
+    const Eigen::Ref<const Eigen::MatrixXd>&, const double&);
+template symbolic::Polynomial EllipsoidPolynomial<symbolic::Variable>(
+    const Eigen::Ref<const VectorX<symbolic::Variable>>&,
+    const Eigen::Ref<const Eigen::VectorXd>&,
+    const Eigen::Ref<const Eigen::MatrixXd>&, const symbolic::Variable&);
+}  // namespace internal
 }  // namespace analysis
 }  // namespace systems
 }  // namespace drake
