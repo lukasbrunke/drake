35904bb83ebe151a9eaf644876858bb3d98356f1
hongkai-dai <daihongkai@gmail.com>
2022-06-17 22:43:55 -0700

    Add quadrotor_test

--- systems/analysis/test/quadrotor.h
+++ systems/analysis/test/quadrotor.h
@@ -2,16 +2,18 @@
 
 #include <math.h>
 
+#include "drake/common/default_scalars.h"
 #include "drake/common/drake_copyable.h"
 #include "drake/common/eigen_types.h"
 #include "drake/common/symbolic.h"
+#include "drake/math/roll_pitch_yaw.h"
 #include "drake/systems/framework/leaf_system.h"
 
 namespace drake {
 namespace systems {
 namespace analysis {
 /**
- * The trigonometric state is (qw-1, qx, qy, qz, p_WB, v_WB, w_WB_B)
+ * The trigonometric state is (qw-1, qx, qy, qz, p_WB, v_WB, omega_WB_B)
  */
 template <typename T>
 class QuadrotorTrigPlant : public LeafSystem<T> {
@@ -60,6 +62,33 @@ template <typename T>
 double EquilibriumThrust(const QuadrotorTrigPlant<T>& quadrotor) {
   return quadrotor.mass() * quadrotor.gravity() / (4 * quadrotor.kF());
 }
+
+symbolic::Polynomial StateEqConstraint(
+    const Eigen::Ref<const Eigen::Matrix<symbolic::Variable, 13, 1>>& x);
+
+/**
+ * Convert from [p_WB, rpy, v_WB, rpyDt] to [qw-1, qx, qy, qz, p_WB, v_WB,
+ * omega_WB_B]
+ */
+template <typename T>
+Eigen::Matrix<T, 13, 1> ToTrigState(
+    const Eigen::Ref<const Eigen::Matrix<T, 12, 1>>& x_original) {
+  Eigen::Matrix<T, 13, 1> x_trig;
+  const math::RollPitchYaw<T> rpy(x_original.template segment<3>(3));
+  const Eigen::Quaternion<T> quaternion = rpy.ToQuaternion();
+  x_trig(0) = quaternion.w() - T(1);
+  x_trig(1) = quaternion.x();
+  x_trig(2) = quaternion.y();
+  x_trig(3) = quaternion.z();
+  x_trig.template segment<3>(4) = x_original.template head<3>();
+  x_trig.template segment<3>(7) = x_original.template segment<3>(6);
+  x_trig.template tail<3>() =
+      rpy.CalcAngularVelocityInChildFromRpyDt(x_original.template tail<3>());
+  return x_trig;
+}
 }  // namespace analysis
 }  // namespace systems
 }  // namespace drake
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class ::drake::systems::analysis::QuadrotorTrigPlant)
