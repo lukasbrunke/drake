d21c15b6e3341c9e72eb1f58788b355dfb71cd41
hongkai-dai <daihongkai@gmail.com>
2022-07-05 16:06:35 -0700

    Use binary search to find initial rho for the cart pole implicit trig dynamics. Still it only verifies small ROA.

--- systems/analysis/test/cart_pole_implicit_trig_clf_demo.cc
+++ systems/analysis/test/cart_pole_implicit_trig_clf_demo.cc
@@ -93,6 +93,72 @@ symbolic::Polynomial AddControlLyapunovConstraint(
   return vdot_sos;
 }
 
+bool FindLagrangian(const Eigen::Matrix<symbolic::Variable, 5, 1>& x,
+                    const Vector2<symbolic::Variable>& z1,
+                    const Vector2<symbolic::Variable>& z2, int lambda0_degree,
+                    const symbolic::Polynomial& V, double rho,
+                    const std::vector<int>& l_degrees,
+                    const std::vector<int>& p_degrees,
+                    const Vector3<symbolic::Polynomial>& qdot, double deriv_eps,
+                    Eigen::Matrix<symbolic::Polynomial, 5, 1> state_constraints,
+                    symbolic::Polynomial* lambda0_sol,
+                    Vector2<symbolic::Polynomial>* l_sol) {
+  solvers::MathematicalProgram prog;
+  prog.AddIndeterminates(x);
+  prog.AddIndeterminates(z1);
+  prog.AddIndeterminates(z2);
+  symbolic::Polynomial lambda0;
+  symbolic::Variables xz_set{x};
+  xz_set.insert(symbolic::Variables(z1));
+  xz_set.insert(symbolic::Variables(z2));
+  std::tie(lambda0, std::ignore) = prog.NewSosPolynomial(
+      xz_set, lambda0_degree,
+      solvers::MathematicalProgram::NonnegativePolynomial::kSos, "Lambda");
+  Vector2<symbolic::Polynomial> l;
+  for (int i = 0; i < 2; ++i) {
+    std::tie(l(i), std::ignore) = prog.NewSosPolynomial(
+        xz_set, l_degrees[i],
+        solvers::MathematicalProgram::NonnegativePolynomial::kSos,
+        "l" + std::to_string(i));
+  }
+  Eigen::Matrix<symbolic::Polynomial, 5, 1> p;
+  for (int i = 0; i < 5; ++i) {
+    p(i) =
+        prog.NewFreePolynomial(xz_set, p_degrees[i], "p" + std::to_string(i));
+  }
+  Vector2<symbolic::Polynomial> z1_poly;
+  Vector2<symbolic::Polynomial> z2_poly;
+  for (int i = 0; i < 2; ++i) {
+    z1_poly(i) = symbolic::Polynomial(z1(i));
+    z2_poly(i) = symbolic::Polynomial(z2(i));
+  }
+  AddControlLyapunovConstraint(&prog, x, z1_poly, z2_poly, lambda0, V, rho, l,
+                               qdot, deriv_eps, p, state_constraints);
+  solvers::SolverOptions solver_options;
+  solver_options.SetOption(solvers::CommonSolverOption::kPrintToConsole, 1);
+  RemoveTinyCoeff(&prog, 1E-9);
+  std::cout << "Smallest coeff in Lagrangian program: " << SmallestCoeff(prog)
+            << "\n";
+  std::cout << "Largest coeff in Lagrangian program: " << LargestCoeff(prog)
+            << "\n";
+  // solver_options.SetOption(solvers::MosekSolver::id(), "writedata",
+  // "cart_pole_implicit_trig_lagrangian.task.gz");
+  const auto result = solvers::Solve(prog, std::nullopt, solver_options);
+  if (result.is_success()) {
+    *lambda0_sol = result.GetSolution(lambda0);
+    const double lsol_tol = 1E-5;
+    *lambda0_sol = lambda0_sol->RemoveTermsWithSmallCoefficients(lsol_tol);
+    for (int i = 0; i < 2; ++i) {
+      (*l_sol)(i) = result.GetSolution(l(i));
+      (*l_sol)(i) = (*l_sol)(i).RemoveTermsWithSmallCoefficients(lsol_tol);
+    }
+    return true;
+  } else {
+    drake::log()->error("Failed to find Lagrangian");
+    return false;
+  }
+}
+
 void Search(const std::optional<std::string>& load_V_init) {
   const CartPoleParams params;
   Eigen::Matrix<symbolic::Variable, 5, 1> x;
@@ -105,7 +171,7 @@ void Search(const std::optional<std::string>& load_V_init) {
                                        symbolic::Variable("z1(1)"));
   const Vector2<symbolic::Variable> z2(symbolic::Variable("z2(0)"),
                                        symbolic::Variable("z2(1)"));
-  const double u_max = 30;
+  const double u_max = 40;
   const Eigen::Matrix<symbolic::Expression, 5, 1> x_expr =
       x.cast<symbolic::Expression>();
   const Matrix2<symbolic::Expression> M_expr =
@@ -140,55 +206,94 @@ void Search(const std::optional<std::string>& load_V_init) {
   }
   const symbolic::Variables x_set{x};
 
-  const double deriv_eps = 0.1;
-  int lambda0_degree = 2;
+  const double deriv_eps = 0.01;
+  int lambda0_degree = 4;
   const std::vector<int> l_degrees{{4, 4}};
-  const std::vector<int> p_degrees{{4, 4, 4, 4, 4}};
+  const std::vector<int> p_degrees{{6, 6, 6, 6, 6}};
 
   double rho_sol;
   if (load_V_init.has_value()) {
     V_init = Load(symbolic::Variables(x), load_V_init.value());
     rho_sol = 1;
   } else {
+    const bool binary_search_rho = true;
     // Maximize rho
-    solvers::MathematicalProgram prog;
-    prog.AddIndeterminates(x);
-    prog.AddIndeterminates(z1);
-    prog.AddIndeterminates(z2);
-    const int d_degree = lambda0_degree / 2 + 1;
-    const symbolic::Variable rho = prog.NewContinuousVariables<1>("rho")(0);
-    symbolic::Polynomial vdot_sos =
-        symbolic::Polynomial(pow(x_expr.dot(x), d_degree)) * (V_init - rho);
-    Vector2<symbolic::Polynomial> l;
-    const RowVector3<symbolic::Polynomial> dVdq = V_init.Jacobian(x.head<3>());
-    for (int i = 0; i < 2; ++i) {
-      std::tie(l(i), std::ignore) = prog.NewSosPolynomial(
-          xz_set, l_degrees[i],
-          solvers::MathematicalProgram::NonnegativePolynomial::kSos, "l");
+    if (binary_search_rho) {
+      double rho_max = 0.01;
+      double rho_min = 1E-4;
+      double rho_tol = 1E-5;
+
+      auto is_rho_feasible = [&x, &z1, &z2, lambda0_degree, &V_init, &l_degrees,
+                              &p_degrees, &qdot, deriv_eps,
+                              &state_constraints](double rho) {
+        symbolic::Polynomial lambda0_sol;
+        Vector2<symbolic::Polynomial> l_sol;
+        return FindLagrangian(x, z1, z2, lambda0_degree, V_init / rho, 1,
+                              l_degrees, p_degrees, qdot, deriv_eps,
+                              state_constraints, &lambda0_sol, &l_sol);
+      };
+      if (is_rho_feasible(rho_max)) {
+        rho_sol = rho_max;
+        V_init = V_init / rho_sol;
+        rho_sol = 1;
+      } else if (!is_rho_feasible(rho_min)) {
+        rho_sol = -kInf;
+        return;
+      } else {
+        while (rho_max - rho_min > rho_tol) {
+          const double rho_mid = (rho_max + rho_min) / 2;
+          std::cout << fmt::format("rho_max={}, rho_min={}, rho_mid={}\n",
+                                   rho_max, rho_min, rho_mid);
+          if (is_rho_feasible(rho_mid)) {
+            rho_min = rho_mid;
+          } else {
+            rho_max = rho_mid;
+          }
+        }
+        rho_sol = rho_min;
+      }
+    } else {
+      solvers::MathematicalProgram prog;
+      prog.AddIndeterminates(x);
+      prog.AddIndeterminates(z1);
+      prog.AddIndeterminates(z2);
+      const int d_degree = lambda0_degree / 2 + 1;
+      const symbolic::Variable rho = prog.NewContinuousVariables<1>("rho")(0);
+      symbolic::Polynomial vdot_sos =
+          symbolic::Polynomial(pow(x_expr.dot(x), d_degree)) * (V_init - rho);
+      Vector2<symbolic::Polynomial> l;
+      const RowVector3<symbolic::Polynomial> dVdq =
+          V_init.Jacobian(x.head<3>());
+      for (int i = 0; i < 2; ++i) {
+        std::tie(l(i), std::ignore) = prog.NewSosPolynomial(
+            xz_set, l_degrees[i],
+            solvers::MathematicalProgram::NonnegativePolynomial::kSos, "l");
+      }
+      const RowVector2<symbolic::Polynomial> dVdv =
+          V_init.Jacobian(x.tail<2>());
+      vdot_sos -= l.sum() * (dVdq.dot(qdot) + deriv_eps * V_init);
+      vdot_sos -= l(0) * dVdv.dot(z1_poly) + l(1) * dVdv.dot(z2_poly);
+      Eigen::Matrix<symbolic::Polynomial, 5, 1> p;
+      for (int i = 0; i < 5; ++i) {
+        p(i) = prog.NewFreePolynomial(xz_set, p_degrees[i],
+                                      "p" + std::to_string(i));
+      }
+      vdot_sos -= p.dot(state_constraints);
+      prog.AddSosConstraint(vdot_sos);
+      prog.AddLinearCost(Vector1d(-1), 0, Vector1<symbolic::Variable>(rho));
+      solvers::SolverOptions solver_options;
+      solver_options.SetOption(solvers::CommonSolverOption::kPrintToConsole, 1);
+      // solver_options.SetOption(solvers::MosekSolver::id(), "writedata",
+      //                         "cart_pole_implicit_trig_max_rho.task.gz");
+      RemoveTinyCoeff(&prog, 1E-9);
+      std::cout << "Smallest coeff: " << SmallestCoeff(prog) << "\n";
+      std::cout << "Largest coeff: " << LargestCoeff(prog) << "\n";
+      const auto result = solvers::Solve(prog, std::nullopt, solver_options);
+      DRAKE_DEMAND(result.is_success());
+      rho_sol = result.GetSolution(rho);
+      std::cout << "V_init <= " << rho_sol << "\n";
+      DRAKE_DEMAND(rho_sol > 0);
     }
-    const RowVector2<symbolic::Polynomial> dVdv = V_init.Jacobian(x.tail<2>());
-    vdot_sos -= l.sum() * (dVdq.dot(qdot) + deriv_eps * V_init);
-    vdot_sos -= l(0) * dVdv.dot(z1_poly) + l(1) * dVdv.dot(z2_poly);
-    Eigen::Matrix<symbolic::Polynomial, 5, 1> p;
-    for (int i = 0; i < 5; ++i) {
-      p(i) =
-          prog.NewFreePolynomial(xz_set, p_degrees[i], "p" + std::to_string(i));
-    }
-    vdot_sos -= p.dot(state_constraints);
-    prog.AddSosConstraint(vdot_sos);
-    prog.AddLinearCost(Vector1d(-1), 0, Vector1<symbolic::Variable>(rho));
-    solvers::SolverOptions solver_options;
-    solver_options.SetOption(solvers::CommonSolverOption::kPrintToConsole, 1);
-    // solver_options.SetOption(solvers::MosekSolver::id(), "writedata",
-    //                         "cart_pole_implicit_trig_max_rho.task.gz");
-    RemoveTinyCoeff(&prog, 1E-9);
-    std::cout << "Smallest coeff: " << SmallestCoeff(prog) << "\n";
-    std::cout << "Largest coeff: " << LargestCoeff(prog) << "\n";
-    const auto result = solvers::Solve(prog, std::nullopt, solver_options);
-    DRAKE_DEMAND(result.is_success());
-    rho_sol = result.GetSolution(rho);
-    std::cout << "V_init <= " << rho_sol << "\n";
-    DRAKE_DEMAND(rho_sol > 0);
   }
 
   const int max_iters = 10;
@@ -196,54 +301,16 @@ void Search(const std::optional<std::string>& load_V_init) {
   symbolic::Polynomial V_sol = V_init;
   const double rho = rho_sol;
   double prev_cost = kInf;
+  std::cout << "start bilinear alternation\n";
   while (iter_count < max_iters) {
     symbolic::Polynomial lambda0_sol;
     Vector2<symbolic::Polynomial> l_sol;
     {
-      solvers::MathematicalProgram prog;
-      prog.AddIndeterminates(x);
-      prog.AddIndeterminates(z1);
-      prog.AddIndeterminates(z2);
-      symbolic::Polynomial lambda0;
-      std::tie(lambda0, std::ignore) = prog.NewSosPolynomial(
-          xz_set, lambda0_degree,
-          solvers::MathematicalProgram::NonnegativePolynomial::kSos, "Lambda");
-      Vector2<symbolic::Polynomial> l;
-      for (int i = 0; i < 2; ++i) {
-        std::tie(l(i), std::ignore) = prog.NewSosPolynomial(
-            xz_set, l_degrees[i],
-            solvers::MathematicalProgram::NonnegativePolynomial::kSos,
-            "l" + std::to_string(i));
-      }
-      Eigen::Matrix<symbolic::Polynomial, 5, 1> p;
-      for (int i = 0; i < 5; ++i) {
-        p(i) = prog.NewFreePolynomial(xz_set, p_degrees[i],
-                                      "p" + std::to_string(i));
-      }
-      AddControlLyapunovConstraint(&prog, x, z1_poly, z2_poly, lambda0, V_sol,
-                                   rho, l, qdot, deriv_eps, p,
-                                   state_constraints);
-      solvers::SolverOptions solver_options;
-      solver_options.SetOption(solvers::CommonSolverOption::kPrintToConsole, 1);
-      RemoveTinyCoeff(&prog, 1E-9);
-      std::cout << "Smallest coeff in Lagrangian program: "
-                << SmallestCoeff(prog) << "\n";
-      std::cout << "Largest coeff in Lagrangian program: " << LargestCoeff(prog)
-                << "\n";
-      // solver_options.SetOption(solvers::MosekSolver::id(), "writedata",
-      // "cart_pole_implicit_trig_lagrangian.task.gz");
-      const auto result = solvers::Solve(prog, std::nullopt, solver_options);
-      if (result.is_success()) {
-        lambda0_sol = result.GetSolution(lambda0);
-        const double lsol_tol = 1E-5;
-        lambda0_sol = lambda0_sol.RemoveTermsWithSmallCoefficients(lsol_tol);
-        for (int i = 0; i < 2; ++i) {
-          l_sol(i) = result.GetSolution(l(i));
-          l_sol(i) = l_sol(i).RemoveTermsWithSmallCoefficients(lsol_tol);
-        }
-      } else {
-        drake::log()->error("Failed to find Lagrangian");
-        std::cout << "rho: " << rho << "\n";
+      const bool found_lagrangian = FindLagrangian(
+          x, z1, z2, lambda0_degree, V_sol, rho, l_degrees, p_degrees, qdot,
+          deriv_eps, state_constraints, &lambda0_sol, &l_sol);
+      if (!found_lagrangian) {
+        std::cout << "rho=" << rho_sol << "\n";
         return;
       }
     }
@@ -301,10 +368,11 @@ void Search(const std::optional<std::string>& load_V_init) {
             "Optimal cost = {}",
             V_sol.EvaluateIndeterminates(x, x_samples).maxCoeff());
 
-        Save(V_sol, "acrobot_implicit_trig_clf.txt");
-        internal::PrintPsdConstraintStat(prog, result_lyapunov);
-        const double curr_cost =
-            V_sol.EvaluateIndeterminates(x, x_samples).maxCoeff();
+        Save(V_sol, "cart_pole_implicit_trig_clf.txt");
+        const Eigen::VectorXd V_at_samples =
+            V_sol.EvaluateIndeterminates(x, x_samples);
+        std::cout << "V at samples: " << V_at_samples.transpose() << "\n";
+        const double curr_cost = V_at_samples.maxCoeff();
         if (curr_cost < prev_cost) {
           prev_cost = curr_cost;
         } else {
