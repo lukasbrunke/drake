1a1faca51173df4a9e5cde975e5b975583eb8383
hongkai-dai <daihongkai@gmail.com>
2022-08-06 16:57:29 -0700

    Pybind more functions in ControlLyapunov. Maximize inner ellipsoid in control barrier by direct maximization.

--- systems/analysis/test/twin_quadrotor2d_trig_cbf_demo.py
+++ systems/analysis/test/twin_quadrotor2d_trig_cbf_demo.py
@@ -9,7 +9,7 @@ from pydrake.solvers.mosek import MosekSolver
 import pydrake.symbolic as sym
 import pydrake.common
 
-def SearchWithSlackA(
+def Search(
     quadrotor: analysis.Quadrotor2dTrigPlant, x: np.array, thrust_max: float,
     deriv_eps: float, unsafe_regions: list, x_safe: np.array
 ) -> sym.Polynomial:
@@ -37,7 +37,7 @@ def SearchWithSlackA(
 
     dynamics_denominator = None
 
-    beta_minus = -0.1 
+    beta_minus = -0.01 
 
     beta_plus = 0.1
 
@@ -49,47 +49,75 @@ def SearchWithSlackA(
     x_set = sym.Variables(x)
 
     #h_init = sym.Polynomial(x[5] * x[5] + x[6] * x[6] - 0.5)
-    with open("twin_quadrotor2d_trig_cbf.pickle", "rb") as input_file:
-        h_init = clf_cbf_utils.deserialize_polynomial(x_set, pickle.load(input_file)["h"])
+    with open("twin_quadrotor2d_trig_cbf2.pickle", "rb") as input_file:
+        h_init = clf_cbf_utils.deserialize_polynomial(
+            x_set, pickle.load(input_file)["h"])
     h_init_x_safe = h_init.EvaluateIndeterminates(x, x_safe)
     print(f"h_init(x_safe): {h_init_x_safe.squeeze()}")
     if np.any(h_init_x_safe < 0):
         h_init -= h_init_x_safe.min()
         h_init += 0.1
 
+    with_slack_a = False
+
     lambda0_degree = 4
     lambda1_degree = 4
     l_degrees = [2] * 16
     hdot_eq_lagrangian_degrees = [h_degree + lambda0_degree - 2] * 2
-    hdot_a_degree = h_degree + lambda0_degree
+    if with_slack_a:
+        hdot_a_degree = h_degree + lambda0_degree
 
     t_degrees = [0]
     s_degrees = [[h_degree - 2]]
     unsafe_eq_lagrangian_degrees = [[h_degree - 2, h_degree - 2]]
-    unsafe_a_degrees = [h_degree]
+    if with_slack_a:
+        unsafe_a_degrees = [h_degree]
     h_x_safe_min = np.array([0.01])
 
-    hdot_a_zero_tol = 3E-9
-    unsafe_a_zero_tol = 1E-9
+    if with_slack_a:
+        hdot_a_zero_tol = 3E-9
+        unsafe_a_zero_tol = 1E-9
+        search_options = analysis.ControlBarrier.SearchWithSlackAOptions(
+            hdot_a_zero_tol, unsafe_a_zero_tol, use_zero_a=True,
+            hdot_a_cost_weight=1., unsafe_a_cost_weight=[1.])
+        search_options.bilinear_iterations = 10
+        search_options.lagrangian_step_solver_options = mp.SolverOptions()
+        search_options.lagrangian_step_solver_options.SetOption(
+            mp.CommonSolverOption.kPrintToConsole, 1)
+        search_result = dut.SearchWithSlackA(
+            h_init, h_degree, deriv_eps, lambda0_degree, lambda1_degree, l_degrees,
+            hdot_eq_lagrangian_degrees, hdot_a_degree, t_degrees, s_degrees,
+            unsafe_eq_lagrangian_degrees, unsafe_a_degrees, x_safe,
+            h_x_safe_min, search_options)
+        search_lagrangian_ret = dut.SearchLagrangian(
+            search_result.h, deriv_eps, lambda0_degree, lambda1_degree, l_degrees,
+            hdot_eq_lagrangian_degrees, None, t_degrees, s_degrees,
+            unsafe_eq_lagrangian_degrees, [None] * len(unsafe_regions),
+            search_options)
+    else:
+        ellipsoids = [analysis.ControlBarrier.Ellipsoid(
+            c=x_safe[:, 0], S=np.eye(12), d=0., r_degree=0,
+            eq_lagrangian_degrees=[0, 0])]
+        ellipsoid_options = [
+            analysis.ControlBarrier.EllipsoidMaximizeOption(
+                t=sym.Polynomial(), s_degree=0, backoff_scale=0.01)]
+        search_options = analysis.ControlBarrier.SearchOptions()
+        search_options.lagrangian_step_solver_options = mp.SolverOptions()
+        search_options.lagrangian_step_solver_options.SetOption(
+            mp.CommonSolverOption.kPrintToConsole, 1)
+        search_options.barrier_step_solver_options = mp.SolverOptions()
+        search_options.barrier_step_solver_options.SetOption(
+            mp.CommonSolverOption.kPrintToConsole, 1)
+        search_options.backoff_scale = 0.02
+        x_anchor = x_safe[:, 0]
+        h_x_anchor_max = 1
+        search_result = dut.Search(
+            h_init, h_degree, deriv_eps, lambda0_degree, lambda1_degree,
+            l_degrees, hdot_eq_lagrangian_degrees, t_degrees, s_degrees,
+            unsafe_eq_lagrangian_degrees, x_anchor, h_x_anchor_max,
+            search_options, ellipsoids, ellipsoid_options)
 
-    search_options = analysis.ControlBarrier.SearchWithSlackAOptions(
-        hdot_a_zero_tol, unsafe_a_zero_tol, use_zero_a=True,
-        hdot_a_cost_weight=1., unsafe_a_cost_weight=[1.])
-    search_options.bilinear_iterations = 10
-    search_options.lagrangian_step_solver_options = mp.SolverOptions()
-    search_options.lagrangian_step_solver_options.SetOption(
-        mp.CommonSolverOption.kPrintToConsole, 1)
-    search_result = dut.SearchWithSlackA(
-        h_init, h_degree, deriv_eps, lambda0_degree, lambda1_degree, l_degrees,
-        hdot_eq_lagrangian_degrees, hdot_a_degree, t_degrees, s_degrees,
-        unsafe_eq_lagrangian_degrees, unsafe_a_degrees, x_safe,
-        h_x_safe_min, search_options)
-    search_lagrangian_ret = dut.SearchLagrangian(
-        search_result.h, deriv_eps, lambda0_degree, lambda1_degree, l_degrees,
-        hdot_eq_lagrangian_degrees, None, t_degrees, s_degrees,
-        unsafe_eq_lagrangian_degrees, [None] * len(unsafe_regions),
-        search_options)
-    with open("twin_quadrotor2d_trig_cbf2.pickle", "wb") as handle:
+    with open("twin_quadrotor2d_trig_cbf3.pickle", "wb") as handle:
         pickle.dump({
             "h": clf_cbf_utils.serialize_polynomial(search_result.h),
             "beta_plus": beta_plus, "beta_minus": beta_minus,
@@ -118,7 +146,7 @@ def DoMain():
         x2 = analysis.ToQuadrotor2dTrigState(safe_states[i][1])
         x_safe[:, i] = np.concatenate((x1[2:], x2[:2] - x1[:2], x2[2:]))
 
-    h_sol = SearchWithSlackA(quadrotor, x, thrust_max, deriv_eps, unsafe_regions, x_safe)
+    h_sol = Search(quadrotor, x, thrust_max, deriv_eps, unsafe_regions, x_safe)
 
 if __name__ == "__main__":
     with MosekSolver.AcquireLicense():
