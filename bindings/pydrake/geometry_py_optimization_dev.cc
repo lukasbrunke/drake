0e49442748762013edd43aa0e33705998ac84e3a
amice <amice@mit.edu>
2022-12-28 12:09:20 -0500

    Bindings written TODO: tests

--- bindings/pydrake/geometry_py_optimization_dev.cc
+++ bindings/pydrake/geometry_py_optimization_dev.cc
@@ -93,11 +93,92 @@ PYBIND11_MODULE(optimization_dev, m) {
                  const Eigen::Ref<const Eigen::VectorXd>&>(),
             py::arg("plant"), py::arg("scene_graph"), py::arg("plane_order"),
             py::arg("q_star"),
-            // Keep alive, reference: `self` keeps `plant` alive.
-//            py::keep_alive<1, 2>(),
             // Keep alive, reference: `self` keeps `scene_graph` alive.
-//            py::keep_alive<1, 3>(),
-                cls_doc.ctor.doc);
+            py::keep_alive<1, 3>(), cls_doc.ctor.doc)
+        .def("rational_forward_kin", &Class::rational_forward_kin,
+            cls_doc.rational_forward_kin.doc)
+        .def("map_geometries_to_separating_planes",
+            &Class::map_geometries_to_separating_planes,
+            cls_doc.map_geometries_to_separating_planes.doc)
+        .def("separating_planes", &Class::separating_planes,
+            cls_doc.separating_planes.doc)
+        .def("y_slack", &Class::y_slack, cls_doc.y_slack.doc)
+        .def(
+            "FindSeparationCertificateGivenPolytope",
+            [](const CspaceFreePolytope* self,
+                const Eigen::Ref<const Eigen::MatrixXd>& C,
+                const Eigen::Ref<const Eigen::VectorXd>& d,
+                const CspaceFreePolytope::IgnoredCollisionPairs&
+                    ignored_collision_pairs,
+                bool search_separating_margin,
+                const CspaceFreePolytope::
+                    FindSeparationCertificateGivenPolytopeOptions& options) {
+              std::unordered_map<SortedPair<geometry::GeometryId>,
+                  CspaceFreePolytope::SeparationCertificateResult>
+                  certificates;
+              bool success = self->FindSeparationCertificateGivenPolytope(C, d,
+                  ignored_collision_pairs, search_separating_margin, options,
+                  &certificates);
+              return std::tie(success, certificates);
+            },
+            py::arg("C"), py::arg("d"), py::arg("ignored_collision_pairs"),
+            py::arg("search_separating_margin"), py::arg("options"));
+
+    py::class_<Class::SeparationCertificateResult>(m,
+        "SeparationCertificateResult", cls_doc.SeparationCertificateResult.doc)
+        .def_readonly("C", &Class::SeparationCertificateResult::C,
+            cls_doc.SeparationCertificateResult.C.doc)
+        .def_readonly("d", &Class::SeparationCertificateResult::d,
+            cls_doc.SeparationCertificateResult.d.doc)
+        .def_readonly("positive_side_lagrangians",
+            &Class::SeparationCertificateResult::positive_side_lagrangians,
+            cls_doc.SeparationCertificateResult.positive_side_lagrangians.doc)
+        .def_readonly("negative_side_lagrangians",
+            &Class::SeparationCertificateResult::negative_side_lagrangians,
+            cls_doc.SeparationCertificateResult.negative_side_lagrangians.doc)
+        .def_readonly("a", &Class::SeparationCertificateResult::a, py_rvp::copy,
+            cls_doc.SeparationCertificateResult.a.doc)
+        .def_readonly("b", &Class::SeparationCertificateResult::b,
+            cls_doc.SeparationCertificateResult.b.doc);
+
+    py::class_<Class::SeparatingPlaneLagrangians>(
+        m, "SeparatingPlaneLagrangians", cls_doc.SeparatingPlaneLagrangians.doc)
+        .def_readonly("polytope_lagrangians",
+            &Class::SeparatingPlaneLagrangians::polytope, py_rvp::copy)
+        .def_readonly("s_lower_lagrangians",
+            &Class::SeparatingPlaneLagrangians::s_lower, py_rvp::copy)
+        .def_readonly("s_upper_lagrangians",
+            &Class::SeparatingPlaneLagrangians::s_upper, py_rvp::copy);
+
+    py::class_<Class::UnitLengthLagrangians>(
+        m, "UnitLengthLagrangians", cls_doc.UnitLengthLagrangians.doc)
+        .def_readonly("polytope_lagrangians",
+            &Class::UnitLengthLagrangians::polytope, py_rvp::copy)
+        .def_readonly("s_lower_lagrangians",
+            &Class::UnitLengthLagrangians::s_lower, py_rvp::copy)
+        .def_readonly("s_upper_lagrangians",
+            &Class::UnitLengthLagrangians::s_upper, py_rvp::copy)
+        .def_readonly("y_lagrangians", &Class::UnitLengthLagrangians::y_square,
+            py_rvp::copy);
+
+    py::class_<Class::FindSeparationCertificateGivenPolytopeOptions>(m,
+        "FindSeparationCertificateGivenPolytopeOptions",
+        cls_doc.FindSeparationCertificateGivenPolytopeOptions.doc)
+        .def_readwrite("num_threads",
+            &Class::FindSeparationCertificateGivenPolytopeOptions::num_threads)
+        .def_readwrite("verbose",
+            &Class::FindSeparationCertificateGivenPolytopeOptions::verbose)
+        .def_readwrite("solver_id",
+            &Class::FindSeparationCertificateGivenPolytopeOptions::solver_id)
+        .def_readwrite("terminate_at_failure",
+            &Class::FindSeparationCertificateGivenPolytopeOptions::
+                terminate_at_failure)
+        .def_readwrite("backoff_scale",
+            &Class::FindSeparationCertificateGivenPolytopeOptions::
+                backoff_scale)
+        .def_readwrite("solver_options",
+            &Class::FindSeparationCertificateGivenPolytopeOptions::
+                solver_options);
   }
   type_visit([m](auto dummy) { DoSeparatingPlaneDeclaration(m, dummy); },
       type_pack<double, symbolic::Variable>());
